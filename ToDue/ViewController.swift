60a42e3cd2de34bec8f8c1a5f65ecf76776ec464
celineou <celineoubaby@gmail.com>
2022-11-10 17:35:07 -0600

    1. changed UI     (1) added table view     (2) added reminder detail page 2. added structure of reminder/ 3. Annotated code from ViewController 4.deleted AddViewController

--- ToDue/ViewController.swift
+++ ToDue/ViewController.swift
@@ -7,50 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate {
+class ViewController: UIViewController {
+    
+    //@Huilin
+    @IBOutlet weak var table: UITableView!
+    var models = [MyReminder]()
     
     
-    @IBAction func addReminders(_ sender: Any) {
-    }
-    
-    @IBOutlet weak var collectionView: UICollectionView!
-    var theData:[event] = []
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        setupCollectionView()
-        
-        DispatchQueue.global(qos: .userInitiated).async {
-            
-            self.fetchDataForcollectionView()
-            
-            DispatchQueue.main.async {
-                self.collectionView.reloadData()
-            }
-        }
+//    @IBAction func addReminders(_ sender: Any) {
+//    }
+//
+//    @IBOutlet weak var collectionView: UICollectionView!
+//    var theData:[event] = []
+//
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//
+//        setupCollectionView()
+//
+//        DispatchQueue.global(qos: .userInitiated).async {
+//
+//            self.fetchDataForcollectionView()
+//
+//            DispatchQueue.main.async {
+//                self.collectionView.reloadData()
+//            }
+//        }
 
-    }
+//    }
 
     //functions
     
     //setupCollectionView
-    func setupCollectionView() {
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        
-        
-    }
-    
-    //fetchDataForcollectionView
-    func fetchDataForcollectionView(){
-        // Currently only for demo, no database query
-        let demoEvent = event(year: 2022, month: 11, day: 11, weekDay: "Friday", title: "CSE599 HW3")
-        theData.append(demoEvent)
-    }
+//    func setupCollectionView() {
+//        collectionView.dataSource = self
+//        collectionView.delegate = self
+//
+//
+//    }
+//
+//    //fetchDataForcollectionView
+//    func fetchDataForcollectionView(){
+//        // Currently only for demo, no database query
+//        let demoEvent = event(year: 2022, month: 11, day: 11, weekDay: "Friday", title: "CSE599 HW3")
+//        theData.append(demoEvent)
+//    }
     
     
 //    func collectionView(_ collectionView: UIcollectionView, didSelectRowAt indexPath: IndexPath) {
@@ -68,15 +72,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 //
 //    }
 
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return theData.count
-    }
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return theData.count
+//    }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! EventCollectionViewCell
-        cell.title.text = theData[indexPath.row].title
-        return cell
-    }
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! EventCollectionViewCell
+//        cell.title.text = theData[indexPath.row].title
+//        return cell
+//    }
 
 }
 
+//@Huilin
+struct MyReminder {
+    let title: String
+    let date: Date
+    let identifier: String
+}
